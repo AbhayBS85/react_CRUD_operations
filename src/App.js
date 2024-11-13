46afe649402ff23f39e81f4db577f64ccfe38336
AbhayBS85 <abskunju@gmail.com>
2024-11-13 18:45:24 +0530

    app.js set successfully

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import StudentForm from './components/User';
 import { BrowserRouter,Route,Routes } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import User from './components/User';
+import AddUser from './components/AddUser';
 
 
 const App=() =>{
   const [users,setUsers]=useState([]);
+  const [editingUser,setEditingUser]=useState(null);
+
   useEffect(() => {
     fetchData();
   },[])
@@ -20,16 +23,88 @@ const App=() =>{
       console.log(err);
     })
   }
+
+  const onAdd = async(name,email) => {
+    await fetch('https://jsonplaceholder.typicode.com/users', {
+      method:'POST',
+      body:JSON.stringify({
+        name:name,
+        email:email,
+      }),
+      headers:{
+        "Content-type": "application/json; charset=UTF-8",
+      }
+    })
+    .then((res) => {
+      if(res.status !== 201){
+        return
+      }else{
+        return res.json();
+      }
+    })
+    .then((data) => {
+      setUsers((users) => [...users,data]);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+
+  const onDelete = async (id) =>{
+    await fetch('https://jsonplaceholder.typicode.com/users/${id}',{
+      method:'DELETE',
+    })
+    .then((res) => {
+      if(res.status !==200){
+        return
+      }else{
+        return setUsers(users.filter((user) => {
+          return user.id !==id;
+        }))
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  };
+  const onEdit = async (id, updatedName, updatedEmail) => {
+    await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        name: updatedName,
+        email: updatedEmail,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+    .then((res) => res.json())
+      .then((updatedUser) => {
+        setUsers(users.map((user) => (user.id === id ? updatedUser : user)));
+        setEditingUser(null); 
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    };
+
   console.log(users)
   return (
     <div className="App">
       <h1>React JS CRUD Operations</h1>
+      <br></br>
+      <AddUser onAdd={onAdd}/>
       <div>
         {
           users.map((user) =>(
-            <User id={user.id} key={user.id} name={user.name} email={user.email}/>
-          ))
-        }
+            <User 
+            id={user.id} 
+            key={user.id} 
+            name={user.name} 
+            email={user.email} 
+            onDelete={onDelete}
+            onEdit={onEdit} />
+          ))}
       </div>
     </div>
   );
--- src/components/User.js
+++ src/components/User.js
@@ -1,17 +1,55 @@
 import React from "react";
 import './user.css';
+import  { useState } from 'react';
 
-const User = ({id,email,name}) =>{
+const User = ({id,email,name,onDelete,onEdit}) =>{
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [editedName, setEditedName] = useState(name);
+    const [editedEmail, setEditedEmail] = useState(email);
+
+    const handleDelete= () => {
+        onDelete(id);
+    };
+
+    const handleEdit = () => {
+        setIsEditing(true);
+    };
+
+    const handleSave = () => {
+        onEdit(id, editedName, editedEmail);
+        setIsEditing(false);
+    };
+
     return (
         <div className="list">
-            <span>{name}</span>
-            <span>{email}</span>
-            <span>
-                <button>Edit</button>
-                <button>Delete</button>
-            </span>
+            {isEditing ? (
+                <>
+                    <input
+                        type="text"
+                        value={editedName}
+                        onChange={(e) => setEditedName(e.target.value)}
+                    />
+                    <input
+                        type="email"
+                        value={editedEmail}
+                        onChange={(e) => setEditedEmail(e.target.value)}
+                    />
+                    <button onClick={handleSave}>Save</button>
+                    <button onClick={() => setIsEditing(false)}>Cancel</button>
+                </>
+            ) : (
+                <>
+                    <span>{name}</span>
+                    <span>{email}</span>
+                    <span>
+                        <button onClick={handleEdit}>Edit</button>
+                        <button onClick={handleDelete}>Delete</button>
+                    </span>
+                </>
+            )}
         </div>
     );
-}
+};
 
 export default User;
